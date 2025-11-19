74d7d928e0b965686494fa6c2df8d2780f0de963
Matheus Thurler <contato@matheusthurler.com.br>
2025-11-19 12:28:39 -0300

    fix

--- variable.tf
+++ variable.tf
@@ -1,11 +1,16 @@
 variable "api_key" {
-  
+  description = "CloudStack API Key"
+  type        = string
+  sensitive   = true
 }
 variable "secret_key" {
-  
+  description = "CloudStack Secret Key"
+  type        = string
+  sensitive   = true
 }
 variable "api_url" {
-  
+  description = "CloudStack API URL"
+  type        = string
 }
 
 
@@ -40,16 +45,30 @@ variable "network_cidr" {
 }
 
 variable "vpc_name" {
-
+  description = "Name for the VPC"
+  type        = string
 }
 
 variable "k8s_semantic_version" {
-
+  description = "The semantic version of the Kubernetes cluster to be deployed."
+  type        = string
 }
-variable "k8s_version_name" {}
-variable "k8s_version_url" {}
-variable "k8s_min_cpu" {}
-variable "k8s_min_memory" {}
+variable "k8s_version_name" {
+  description = "The name of the Kubernetes version in CloudStack."
+  type        = string
+}
+variable "k8s_version_url" {
+  description = "The URL of the Kubernetes version template."
+  type        = string
+}
+variable "k8s_min_cpu" {
+  description = "The minimum number of CPUs required for the Kubernetes version."
+  type        = number
+}
+variable "k8s_min_memory" {
+  description = "The minimum memory required for the Kubernetes version."
+  type        = number
+}
 
 variable "k8s_cluster_name" {
   description = "Name of the Kubernetes cluster"
@@ -71,5 +90,6 @@ variable "k8s_worker_nodes_size" {
   type        = number
 }
 variable "network_offering_name" {
-  
+  description = "Name of the network offering for the guest network."
+  type        = string
 }
