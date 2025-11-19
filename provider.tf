f2621f9933a6396e0bcd5d5d0180f4408913c7a2
Matheus Thurler <contato@matheusthurler.com.br>
2025-11-19 12:27:20 -0300

    fix

--- provider.tf
+++ provider.tf
@@ -9,7 +9,7 @@ terraform {
 
 
 provider "cloudstack" {
-  api_url    = "http://192.168.68.103:8080/client/api"
-  api_key    = "U_peugiJJz7IOlsOjHIBH_FlcbCp-e1shduW2uyMqjyDe4u0dawXlQXsUxfmJgMFkZ_33EjGuE4Fhua_4n1XRw"
-  secret_key = "_GYTwOvQc5qzXlUudgg0GNeqpLzkQhNBfspatkgacIsBusEQH7VMFBEUPxfHeDHUQE9i_8qpMAu9s0VrcU3vLg"
+  api_url    = var.api_url
+  api_key    = var.api_key
+  secret_key = var.secret_key
 }
